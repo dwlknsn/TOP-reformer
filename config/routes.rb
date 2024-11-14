68ebd38de908af5ddda03793be99b86b4a01cf60
dwlknsn <dwlknsn@users.noreply.github.com>
2024-11-14 12:13:13 +0700

    Add edit + udpate actions and display errors when create/update fails

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root "users#new"
-  resources :users, only: [ :new, :create ]
+  resources :users, only: [ :new, :create, :edit, :update ]
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
